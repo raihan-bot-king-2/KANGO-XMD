3405f720c7ea0c00d3ef73d7dfba72e47800fefc
raihan-bot-king-2 <raihanenterprise09@gmail.com>
2025-05-18 21:06:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~MylXzJaB#yVqIEW5Zp_hmdimSSk-6Bd6orzzfI4UedCLsvV6zeBw' 
 //Enter your 𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿 session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
